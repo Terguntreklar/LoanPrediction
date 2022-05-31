906f994540ab3effcc819149844081570621ca35
Aly Mohamed <69851232+Terguntreklar@users.noreply.github.com>
2022-05-31 02:18:41 +0200

    Update accuracy_test.py

--- accuracy_test.py
+++ accuracy_test.py
@@ -7,7 +7,7 @@ from sklearn.metrics import accuracy_score
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
-def divide_columns_by_max(dataframe):
+def divide_columns_by_max(dataframe): #Data scaling [0-50]
     newFrame = []
     for row in np.array(dataframe.values).T:
         newFrame.append([(item*50.0)/max(row)*1.0 for item in row])
@@ -24,29 +24,28 @@ def accuracy_tests(TEST_NUMBER,test_train_subsets):
         print('Accuracy of ',model[0],' = ',model[2])
 def feature_selection_tests(dataset, number_of_features,test_train_subsets):
     Feature_Models = [["Linear SVM",SVC(kernel='linear'),0],["LR",LogisticRegression(max_iter=1000),0],["Decision Tree",DecisionTreeClassifier(),0]]
-    for model in Feature_Models:
+    for model in Feature_Models: #apply RFE to get feature importance
         for i in range(0,1):
             model[1] = RFE(model[1],n_features_to_select=number_of_features)
             model[1] = model[1].fit(test_train_subsets[i][0],test_train_subsets[i][2])
             model[2] += model[1].ranking_
-    data_sets_feature_selected = []
-    for feature_model in Feature_Models:
+    data_sets_feature_selected = [] #this will hold different feature sets
+    for feature_model in Feature_Models: #cut the original dataset 
         x = ([i for i in range(len(feature_model[2])) if (feature_model[2][i]==1)])
         y = list(dataset.keys()[:-1][x])
         y.append('Loan_Status')
-        #dataset = dataset[['LoanAmount','CoapplicantIncome','Property_Area','ApplicantIncome','Credit_History','Loan_Amount_Term','Loan_Status']]
         dt = dataset[y]
         data_sets_feature_selected.append(divide_columns_by_max(dt))
 
     models = [["SVM",SVC(kernel='poly',degree=12)],["LR",LogisticRegression(max_iter=1000)],["NB",GaussianNB()],["KNN",KNeighborsClassifier()]]
-    test_data = []
-    for feature_dataset in data_sets_feature_selected:
+    test_data = [] #this holds testing and training datasets
+    for feature_dataset in data_sets_feature_selected: #create testing and training datasets
         arr = feature_dataset
         x = arr[:,0:-1]
         y = arr[:,-1]
         a = list(train_test_split(x,y,test_size=0.2))
         test_data.append(a)
-    for testing_sub_dataset in test_data:
+    for testing_sub_dataset in test_data: #finally, calculate accuracy
         for model in models:
             model[1] = model[1].fit(testing_sub_dataset[0],testing_sub_dataset[2])
             y_pred = model[1].predict(testing_sub_dataset[1])
